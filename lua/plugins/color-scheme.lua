cad834cd19a74942ce0c1162d8eb3b564bced7bf
Cheick Diarra <cheickdiarra74@gmail.com>
2024-01-02 09:08:32 +0100

    Switch color scheme to github light

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,5 +1,8 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night", light_style = "day" }
+  "projekt0n/github-nvim-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd("colorscheme github_light")
+  end,
 }
