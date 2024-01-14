73d82d9e30a6c0198b6dc672602efcdd37978694
Diachk <cheickdiarra74@gmail.com>
2024-01-14 10:35:15 +0100

    remove notify bg warning

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,3 +1,4 @@
+local notify = "notify"
 return {
   "projekt0n/github-nvim-theme",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -6,9 +7,12 @@ return {
     require("github-theme").setup({
       options = {
         transparent = true,
-        dim_inactive = true,
+        dim_inactive = false,
       },
     })
     vim.cmd("colorscheme github_light")
+    require(notify).setup({
+      background_colour = "#fefefe",
+    })
   end,
 }
