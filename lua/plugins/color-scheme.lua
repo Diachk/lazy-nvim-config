d05a1cc9506e6e11caea318a8815a1c2e26f427c
Cheick Diarra <cheickdiarra74@gmail.com>
2024-05-08 15:35:52 +0200

    replace github with modus

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,48 +1,49 @@
 return {
-  "projekt0n/github-nvim-theme",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "miikanissi/modus-themes.nvim",
+  lazy = false,
+  priority = 1000,
   config = function()
-    require("github-theme").setup({
-      options = {
-        transparent = true,
-        dim_inactive = false,
+    -- Default options
+    require("modus-themes").setup({
+      -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+      -- `auto` will automatically set style based on background set with vim.o.background
+      style = "auto",
+      variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+      transparent = true, -- Transparent background (as supported by the terminal)
+      dim_inactive = false, -- "non-current" windows are dimmed
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
       },
-      palettes = {
-        github_light_default = {
-          orange = "#F8F9FA",
-        },
-      },
-      specs = {
-        github_light = {
-          -- bg0 = "#FF0000", -- works, set leader menu
-          -- bg1 = "#FF0000", -- works, sets status line
-          -- bg3 = "#FF0000", -- works, set cursor line
-          syntax = { -- Syntax works without anything in addition
-            builtin1 = "#C92785",
-            -- builtin0 = "#C92785",
-            -- builtin2 = "#C92785",
-            type = "#707174",
-            const = "#C92785",
-            string = "#B31512",
-            -- field = "#00FF00",
-            -- ident = "#00FF00",
-            -- variable = "#00FF00",
-            -- bracket = "#309331",
-            func = "#202124",
-            keyword = "#175ABC", -- This works
-            comment = "#137333", -- This works
-          },
-        },
-      },
-      groups = {
-        github_light = {
-          -- Normal = { bg = "#F8F9FA" },
-          -- NormalFloat = { bg = "#FFFFFF" },
-        },
-      },
+
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors)
+        colors.bg_active = "#ffffff" -- changed from #e0e0e0, we also like #f8f9fa
+        colors.visual = "#72f64a"
+      end,
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors)
+        highlights.Pmenu = { bg = colors.bg_alt }
+        highlights.PmenuSel = { bg = colors.bg_hl_line }
+        highlights.PmenuExtra = { bg = colors.bg_alt }
+        highlights.FloatBorder = { bg = colors.bg_alt }
+        highlights.NormalFloat = { bg = "#f8f9fa" }
+        highlights.CursorLine = { bg = "#effaff" }
+        highlights.Directory = { fg = "#000000" }
+      end,
     })
-    vim.cmd("colorscheme github_light")
+
+    vim.cmd("colorscheme modus")
     vim.cmd("set nolist")
   end,
 }
