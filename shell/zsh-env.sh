9e9c48a5db08c17e7ca60c4459f4607a53bc83be
Diachk <cheickdiarra74@gmail.com>
2024-09-06 14:45:27 +0200

    add zsh config

---
+++ shell/zsh-env.sh
@@ -0,0 +1,9 @@
+# GNU coreutils
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+
+# WebStorm CLI
+export PATH="/Applications/WebStorm.app/Contents/MacOS:$PATH"
+
+# Chromium location
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+export PUPPETEER_EXECUTABLE_PATH=$(which chromium)
